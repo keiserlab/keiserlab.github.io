25aa9f313e5f18da8af2b21e613419617337e9b5
feedlydummy <feedly@keiserlab.org>
2018-04-12 12:34:55 -0700

    Feedly auto-commit: Thu Apr 12 12:34:55 PDT 2018

--- feedly_setup.sh
+++ feedly_setup.sh
@@ -13,28 +13,11 @@ archive=$JBARCHIVE_LOC"/archive";
 # has last update, has log, archives
 source activate py3env
 python $run_py $peeps $jname $users_per_article -p $last_update_file -l $log_file -a $archive -o days=-1 -s weeks=-1; 
-if [[ $? != 0 ]]; then
-   mail -s "Feedly XML run failed" $admin_email
-   exit 1
-fi
 
 # Finally, update the github
 git add *
-if [[ $? != 0 ]]; then 
-   mail -s "Feedly add failed" $admin_email
-   exit 1
-fi
 
 git commit -a -m "Feedly auto-commit: $(date)"
-if [[ $? != 0 ]]; then 
-   mail -s "Feedly commit failed" $admin_email
-   exit 1
-fi
 
 git push --all origin
-if [[ $? != 0 ]]; then 
-   mail -s "Feedly push failed" $admin_email
-   exit 1
-fi
-
-# mail -s "Feedly push ok" $admin_email
+source deactivate
--- update_feedly.sh
+++ update_feedly.sh
@@ -12,31 +12,13 @@ archive=$JBARCHIVE_LOC"/archive";
 
 # Call jbdistributor
 # has last update, has log, archives
+source activate py3env
 python $run_py $peeps $jname $users_per_article -p $last_update_file -l $log_file -a $archive ; 
 
-if [[ $? != 0 ]] then
-   mail -s "Feedly XML run failed" $admin_email
-   exit 1
-fi
-
-# Finally, update the github
 git add *
-if [[ $? != 0 ]] then 
-   mail -s "Feedly add failed" $admin_email
-   exit 1
-fi
 
 git commit -a -m "Feedly auto-commit: $(date)"
-if [[ $? != 0 ]] then 
-   mail -s "Feedly commit failed" $admin_email
-   exit 1
-fi
 
 git push --all origin
-if [[ $? != 0 ]] then 
-   mail -s "Feedly push failed" $admin_email
-   exit 1
-fi
 
-mail -s "Feedly push ok" $admin_email
-
+source deactivate
