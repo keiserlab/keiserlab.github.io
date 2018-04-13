f1550e8e5464ea41c28de49597aea6e0e0ea6b8b
feedlydummy <feedly@keiserlab.org>
2018-04-12 17:40:58 -0700

    Feedly auto-commit: Thu Apr 12 17:40:58 PDT 2018

---
+++ update_feedly.bash
@@ -0,0 +1,24 @@
+#/bin/bash
+
+admin_email='ecaceres@keiserlab.org';
+run_py=$JBDISTRIBUTOR"/jbdistributor/jbdistributor.py";
+peeps=$JBDISTRIBUTOR"/data/peeps.tsv";
+jname="journalblitz";
+users_per_article="3";
+last_update_file=$JBARCHIVE_LOC"/last_update.pkl";
+log_file=$JBARCHIVE_LOC"/log.tsv";
+archive=$JBARCHIVE_LOC"/archive";
+
+
+# Call jbdistributor
+# has last update, has log, archives
+source /srv/home/ecaceres/anaconda2/envs/py3env/bin/activate /srv/home/ecaceres/anaconda2/envs/py3env
+python $run_py $peeps $jname $users_per_article -p $last_update_file -l $log_file -a $archive ; 
+
+git add *
+
+git commit -a -m "Feedly auto-commit: $(date)"
+
+git push --all origin
+
+source deactivate
